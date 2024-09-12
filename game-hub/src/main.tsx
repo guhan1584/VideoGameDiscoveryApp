5550ba0b852335b8e5ed0647d9ca4c03f775999f
guhan1584 <guhan4321@gmail.com>
2024-09-12 15:27:38 +0300

    Installing react query and devtools

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import theme from "./theme.ts";
 import App from "./App.tsx";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,7 +14,9 @@ createRoot(document.getElementById("root")!).render(
       <ColorModeScript
         initialColorMode={theme.config.initialColorMode}
       ></ColorModeScript>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>
 );
