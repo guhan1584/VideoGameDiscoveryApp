cd713f591d3fa8b94fcb6221088aef03db6b5e54
guhan1584 <guhan4321@gmail.com>
2024-09-13 00:17:52 +0300

    Fetching platforms

--- game-hub/src/hooks/usePlatforms.ts
+++ game-hub/src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import { FetchingResponse } from "./useData";
+import apiClient from "../services/api-client";
+import platforms from "../data/platforms";
 
 interface Platform {
   id: number;
@@ -6,6 +9,18 @@ interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchingResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: {
+      count: platforms.results.length,
+      results: platforms.results,
+    },
+  });
 
 export default usePlatforms;
