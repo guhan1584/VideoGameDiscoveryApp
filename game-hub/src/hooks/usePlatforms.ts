f5b742dc5e69176d797327b976400d7a6272c637
guhan1584 <guhan4321@gmail.com>
2024-09-13 00:48:41 +0300

    Removing duplicates interfaces

--- game-hub/src/hooks/usePlatforms.ts
+++ game-hub/src/hooks/usePlatforms.ts
@@ -3,7 +3,7 @@ import { FetchingResponse } from "../services/api-client";
 import apiClient from "../services/api-client";
 import platforms from "../data/platforms";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
