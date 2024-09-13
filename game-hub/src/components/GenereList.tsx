3d0f2febbc415a18c3ff7df17ea57c105885bc30
guhan1584 <guhan4321@gmail.com>
2024-09-13 15:25:01 +0300

    Simplifying query objects

--- game-hub/src/components/GenereList.tsx
+++ game-hub/src/components/GenereList.tsx
@@ -12,10 +12,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenereList = ({ selectedGenre, onSelectGenre }: Props) => {
+const GenereList = ({ selectedGenreId, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -37,7 +37,7 @@ const GenereList = ({ selectedGenre, onSelectGenre }: Props) => {
                 objectFit={"cover"}
               />
               <Button
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                fontWeight={genre.id === selectedGenreId ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 variant={"link"}
                 fontSize={"lg"}
