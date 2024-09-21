1c396e01d0df9bf1f25bbad226f285609b273039
guhan1584 <guhan4321@gmail.com>
2024-09-22 01:26:32 +0300

    Setting up routing

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -4,8 +4,9 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import theme from "./theme.ts";
-import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.tsx";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +17,7 @@ createRoot(document.getElementById("root")!).render(
         initialColorMode={theme.config.initialColorMode}
       ></ColorModeScript>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
